d1df1358bcd6d510d92fdff87812dcee93874675
Jakub Supa <jakub.supa@icloud.com>
2021-10-10 18:48:52 +0200

    🚀 init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const server = require('./lib/server');
+const helpers = require('./lib/helpers');
+
+const config = require('./lib/config');
+
+const app = {};
+
+app.init = () => {
+    if (config.allowWebServer === true) {
+        server.init();
+    } else {
+        console.log(`[ ${helpers.getTime()} ] : Web server is disabled`);
+    }
+    // workers.init();
+};
+
+app.init();
+
+module.exports = app;
